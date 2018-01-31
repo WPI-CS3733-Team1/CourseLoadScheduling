8fba4442fe14bdd46dbda6187706c2a71daaaccc
Surya Vadivazhagu <vsuryav@gmail.com>
2018-01-31 15:11:52 -0500

    Added CoursesExtractor DepartmentsExtractor Edit to: UsersRolesLinksExtractor, adding the deleted boolean value.

--- src/main/java/org/dselent/scheduling/server/extractor/DepartmentsExtractor.java
+++ src/main/java/org/dselent/scheduling/server/extractor/DepartmentsExtractor.java
@@ -2,17 +2,42 @@ package org.dselent.scheduling.server.extractor;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.dselent.scheduling.server.model.Department;
+import org.dselent.scheduling.server.model.User;
 
 public class DepartmentsExtractor extends Extractor<List<Department>> {
 
 	
 	@Override
 	public List<Department> extractData(ResultSet rs) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		{
+			List<Department> resultList = new ArrayList<>();
+
+			while(rs.next())
+			{
+				Department result = new Department();
+					
+				result.setId(rs.getInt(Department.getColumnName(Department.Columns.ID)));
+				
+				if(rs.wasNull())
+				{
+					result.setId(null);
+				}
+				result.setDepartmentID(rs.getString(Department.getColumnName(Department.Columns.DEPARTMENT_ID)));
+				
+				if(rs.wasNull())
+				{
+					result.setDepartmentID(null);
+				}
+				resultList.add(result);
+			}
+				
+			return resultList;
+		}
+
 	}
 
 }
