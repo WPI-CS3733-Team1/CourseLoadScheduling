b4444cc137e668919aaba685e5e0126d859d9c47
bwagner <bpwagner15@wpi.edu>
2018-02-02 12:42:43 -0500

    Ben- custom Queries connected

--- src/main/java/org/dselent/scheduling/server/miscellaneous/QueryPathConstants.java
+++ src/main/java/org/dselent/scheduling/server/miscellaneous/QueryPathConstants.java
@@ -20,13 +20,21 @@ public class QueryPathConstants
 	private static String SQL_EXTENSION = ".sql";
 
 	private static String USERS_WITH_ROLE_PATH = BASE_QUERY_PATH + "CustomUsersWithRole" + SQL_EXTENSION;
-
+	private static String FETCH_SCHEDULE_PATH = BASE_QUERY_PATH + "FetchSchedule" + SQL_EXTENSION;
+	private static String CHANGE_PASSWORD_PATH = BASE_QUERY_PATH + "ChangePassword" + SQL_EXTENSION;
+	private static String REQUEST_CHANGE_PATH = BASE_QUERY_PATH + "RequestChange" + SQL_EXTENSION;
+	private static String RESOLVE_MESSAGE_PATH = BASE_QUERY_PATH + "ResolveMessage" + SQL_EXTENSION;
+	private static String SEARCH_ACADEMIC_TERM_PATH = BASE_QUERY_PATH + "SearchAcademicTerm" + SQL_EXTENSION;
 	
 	/////////////////////////////////////////////////////////////////////////////////////////////////
 	
 
 	public static String USERS_WITH_ROLE_QUERY = readFile(USERS_WITH_ROLE_PATH);
-
+	public static String FETCH_SCHEDULE_QUERY = readFile(FETCH_SCHEDULE_PATH);
+	public static String CHANGE_PASSWORD_QUERY = readFile(CHANGE_PASSWORD_PATH);
+	public static String REQUEST_CHANGE_QUERY = readFile(REQUEST_CHANGE_PATH);
+	public static String RESOLVE_MESSAGE_QUERY = readFile(RESOLVE_MESSAGE_PATH);
+	public static String SEARCH_ACADEMIC_TERM_QUERY = readFile(SEARCH_ACADEMIC_TERM_PATH);
 	
 	private QueryPathConstants()
 	{
