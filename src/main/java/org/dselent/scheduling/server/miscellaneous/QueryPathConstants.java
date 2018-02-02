7ad3f9ad51c0a0f375c246efc0c6da2707c8b075
Harsh Rana <hr23232323@gmail.com>
2018-02-02 13:13:57 -0500

    Harsh: Resolving issues with custom queries

--- src/main/java/org/dselent/scheduling/server/miscellaneous/QueryPathConstants.java
+++ src/main/java/org/dselent/scheduling/server/miscellaneous/QueryPathConstants.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.dselent.scheduling.server.miscellaneous;
 
 import java.io.File;
@@ -15,62 +14,8 @@ import org.springframework.core.io.Resource;
  * @author dselent
  *
  */
-public class QueryPathConstants
-{
-	private static String BASE_QUERY_PATH = "sql" + File.separator + "Active" + File.separator;
-	private static String SQL_EXTENSION = ".sql";
 
-	private static String USERS_WITH_ROLE_PATH = BASE_QUERY_PATH + "CustomUsersWithRole" + SQL_EXTENSION;
 
-	
-	
-	/////////////////////////////////////////////////////////////////////////////////////////////////
-	
-
-	public static String USERS_WITH_ROLE_QUERY = readFile(USERS_WITH_ROLE_PATH);
-
-	
-	
-	private QueryPathConstants()
-	{
-		
-	}
-	
-	public static String readFile(String path)
-	{
-		byte[] encodedbytes = null;
-		
-		try
-		{
-			Resource resource = new ClassPathResource(path);
-			encodedbytes = Files.readAllBytes(Paths.get(resource.getURI()));
-		}
-		catch(IOException ioe)
-		{
-			ioe.printStackTrace();
-		}
-		
-		return new String(encodedbytes);
-	}
-
-}
-=======
-package org.dselent.scheduling.server.miscellaneous;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-
-/**
- * Register all custom SQL query files here
- * 
- * @author dselent
- *
- */
 public class QueryPathConstants
 {
 	private static String BASE_QUERY_PATH = "sql" + File.separator + "Active" + File.separator;
@@ -116,4 +61,3 @@ public class QueryPathConstants
 	}
 
 }
->>>>>>> b4444cc137e668919aaba685e5e0126d859d9c47
