aae356fd4dd4d54639be0515ac7270e1a69ef34a
bwagner <bpwagner15@wpi.edu>
2018-02-02 21:51:12 -0500

    Added CompleteSectionsExtractor

---
+++ src/main/java/org/dselent/scheduling/server/extractor/CompleteSectionsExtractor.java
@@ -0,0 +1,50 @@
+package org.dselent.scheduling.server.extractor;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.dselent.scheduling.server.model.CompleteSection;
+
+public class CompleteSectionsExtractor extends Extractor<List<CompleteSection>>
+{
+	@Override
+	public List<CompleteSection> extractData(ResultSet rs) throws SQLException
+	{
+		List<CompleteSection> resultList = new ArrayList<>();
+
+		while(rs.next())
+		{
+			CompleteSection result = new CompleteSection();
+				
+			result.setId(rs.getInt(CompleteSection.getColumnName(CompleteSection.Columns.ID)));
+			
+			if(rs.wasNull())
+			{
+				result.setId(null);
+			}
+			
+			result.setAcademicTerm(rs.getString(CompleteSection.getColumnName(CompleteSection.Columns.ACADEMIC_TERM)));
+			result.setAcadmicYear(rs.getInt(CompleteSection.getColumnName(CompleteSection.Columns.ACADEMIC_YEAR)));
+			result.setCourseLocation(rs.getString(CompleteSection.getColumnName(CompleteSection.Columns.COURSE_LOCATION)));
+			result.setCourseNum(rs.getString(CompleteSection.getColumnName(CompleteSection.Columns.COURSE_NUMBER)));
+			result.setCrn(rs.getInt(CompleteSection.getColumnName(CompleteSection.Columns.CRN)));
+			result.setDaysPerWeek(rs.getString(CompleteSection.getColumnName(CompleteSection.Columns.DAYS_PER_WEEK)));
+			result.setDeptId(rs.getInt(CompleteSection.getColumnName(CompleteSection.Columns.DEPT_ID)));
+			result.setEndTime(rs.getInt(CompleteSection.getColumnName(CompleteSection.Columns.END_TIME)));
+			result.setExpectedPopulation(rs.getInt(CompleteSection.getColumnName(CompleteSection.Columns.EXPECTED_POPULATION)));
+			result.setRequiredFrequency(rs.getInt(CompleteSection.getColumnName(CompleteSection.Columns.REQUIRED_FREQUENCY)));
+			result.setSectionId(rs.getInt(CompleteSection.getColumnName(CompleteSection.Columns.SECTION_ID)));
+			result.setSectionName(rs.getString(CompleteSection.getColumnName(CompleteSection.Columns.SECTION_NAME)));
+			result.setStartTime(rs.getInt(CompleteSection.getColumnName(CompleteSection.Columns.START_TIME)));
+			result.setName(rs.getString(CompleteSection.getColumnName(CompleteSection.Columns.NAME)));
+			
+		
+			resultList.add(result);
+		}
+			
+		return resultList;
+	}
+
+}
