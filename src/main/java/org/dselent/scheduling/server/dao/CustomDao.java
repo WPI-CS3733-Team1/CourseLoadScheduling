f7cc2884506701502262c0501cb67cf8fef923ee
noah parker <nparker@wpi.edu>
2018-02-02 21:53:45 -0500

    Noah - compiled all custom query functions in CustomDaoImpl.java

--- src/main/java/org/dselent/scheduling/server/dao/CustomDao.java
+++ src/main/java/org/dselent/scheduling/server/dao/CustomDao.java
@@ -2,6 +2,8 @@ package org.dselent.scheduling.server.dao;
 
 import java.util.List;
 
+import org.dselent.scheduling.server.model.Course;
+import org.dselent.scheduling.server.model.Section;
 import org.dselent.scheduling.server.model.User;
 import org.springframework.stereotype.Repository;
 
@@ -16,4 +18,30 @@ public interface CustomDao
 {
 	
 	public List<User> getAllUsersWithRole(int roleId);
+
+	public List<Course> getAllCourseWithNumber(String courseNumber);
+
+	List<Section> getAvailableSectionsInDept(int deptID);
+
+	List<Section> getSectionByDeptId(int deptID);
+
+	List<User> searchUserByUsername(String userName);
+
+	List<Section> getAllSectionsInAcademicTerm(String academicTerm);
+
+	List<Section> getSectionsWithStartTime(int startTime);
+
+	List<Section> getAllSectionsInAcademicYear(int academicYear);
+
+	List<Section> getSectionsWithReqFreq(int reqFreq);
+
+	List<Course> getCourseByName(String courseName);
+
+	List<Section> getByCrn(int CRN);
+
+	List<Section> getAllSectionsWithEndTime(int endTime);
+
+	List<Section> getAllSectionsWithExpectedPopulation(int expectedPopulation);
+
+	List<User> getUser(String userName);
 }
--- src/main/java/org/dselent/scheduling/server/dao/impl/CustomDaoImpl.java
+++ src/main/java/org/dselent/scheduling/server/dao/impl/CustomDaoImpl.java
@@ -3,9 +3,11 @@ package org.dselent.scheduling.server.dao.impl;
 import java.util.List;
 
 import org.dselent.scheduling.server.dao.CustomDao;
+import org.dselent.scheduling.server.extractor.CoursesExtractor;
 import org.dselent.scheduling.server.extractor.SectionsExtractor;
 import org.dselent.scheduling.server.extractor.UsersExtractor;
 import org.dselent.scheduling.server.miscellaneous.QueryPathConstants;
+import org.dselent.scheduling.server.model.Course;
 import org.dselent.scheduling.server.model.Section;
 import org.dselent.scheduling.server.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +35,7 @@ public class CustomDaoImpl implements CustomDao
 	    return usersWithRoleList;
 	}
 
+	@Override
 	public List<Section> getAvailableSectionsInDept(int deptID) {
 		SectionsExtractor extractor = new SectionsExtractor();
 		String queryTemplate = new String(QueryPathConstants.FETCH_SECTIONS_QUERY);
@@ -43,6 +46,7 @@ public class CustomDaoImpl implements CustomDao
 		return availableSections;
 	}
 	
+	@Override
 	public List<Section> getSectionByDeptId(int deptID) {
 		SectionsExtractor extractor = new SectionsExtractor();
 		String queryTemplate = new String(QueryPathConstants.SEARCH_DEPT_ID_QUERY);
@@ -53,5 +57,127 @@ public class CustomDaoImpl implements CustomDao
 		return availableSections;
 	}
 	
+	@Override
+	public List<User> searchUserByUsername(String userName)
+	{
+		UsersExtractor extractor = new UsersExtractor();
+		String queryTemplate = new String(QueryPathConstants.SEARCH_USERS_QUERY);
+		MapSqlParameterSource parameters = new MapSqlParameterSource();
+		parameters.addValue("userName", userName );
+		List<User> searchedUsers = namedParameterJdbcTemplate.query(queryTemplate, parameters, extractor);
+		return searchedUsers;
+	}
+	
+	@Override
+	public List<Section> getAllSectionsInAcademicTerm(String academicTerm)
+	{
+		SectionsExtractor extractor = new SectionsExtractor();
+		String queryTemplate = new String(QueryPathConstants.SEARCH_ACADEMIC_TERM_QUERY);
+		MapSqlParameterSource parameters = new MapSqlParameterSource();
+		parameters.addValue("academicTerm", academicTerm);
+		List<Section> sectionsInAcademicTermsList = namedParameterJdbcTemplate.query(queryTemplate, parameters, extractor);
+		return sectionsInAcademicTermsList;
+	}
+	
+	@Override
+	public List<Section> getSectionsWithStartTime(int startTime)
+	{
+		SectionsExtractor extractor = new SectionsExtractor();
+		String queryTemplate = new String(QueryPathConstants.SEARCH_START_TIME_QUERY);
+		MapSqlParameterSource parameters = new MapSqlParameterSource();
+		parameters.addValue("startTime", startTime);
+		List<Section> sectionsWithStartTime = namedParameterJdbcTemplate.query(queryTemplate, parameters, extractor);
+		return sectionsWithStartTime;
+	}
+	
+	@Override
+	public List<Section> getAllSectionsInAcademicYear(int academicYear)
+	{
+		SectionsExtractor extractor = new SectionsExtractor();
+		String queryTemplate = new String(QueryPathConstants.SEARCH_ACADEMIC_YEAR_QUERY);
+		MapSqlParameterSource parameters = new MapSqlParameterSource();
+		parameters.addValue("academicYear", academicYear);
+		List<Section> sectionsInAcademicYearList = namedParameterJdbcTemplate.query(queryTemplate, parameters, extractor);
+		return sectionsInAcademicYearList;
+	}
+
+	@Override
+	public List<Section> getSectionsWithReqFreq(int reqFreq)
+	{
+		SectionsExtractor extractor = new SectionsExtractor();
+		String queryTemplate = new String(QueryPathConstants.SEARCH_REQUIRED_FREQUENCY_QUERY);
+		MapSqlParameterSource parameters = new MapSqlParameterSource();
+		parameters.addValue("requiredFrequency", reqFreq);
+		List<Section> sectionsWithReqFreg = namedParameterJdbcTemplate.query(queryTemplate, parameters, extractor);
+		return sectionsWithReqFreg;
+	}
+	
+	@Override
+	public List<Course> getCourseByName(String courseName)
+	{
+		CoursesExtractor extractor = new CoursesExtractor();
+		String queryTemplate = new String(QueryPathConstants.SEARCH_COURSE_NAME_QUERY);
+		MapSqlParameterSource parameters = new MapSqlParameterSource();
+		parameters.addValue("courseName", courseName);
+		List<Course> searchedCourses = namedParameterJdbcTemplate.query(queryTemplate, parameters, extractor);
+		return searchedCourses;	
+	}
+	
+	
+	@Override
+	public List<Section> getByCrn(int CRN)
+	{
+		SectionsExtractor extractor = new SectionsExtractor();
+		String queryTemplate = new String(QueryPathConstants.SEARCH_CRN_QUERY);
+		MapSqlParameterSource parameters = new MapSqlParameterSource();
+		parameters.addValue("CRN", CRN);
+		List<Section> sectionsWithCrn = namedParameterJdbcTemplate.query(queryTemplate, parameters, extractor);
+		return sectionsWithCrn;
+		
+	}
+	
+	@Override
+	public List<Course> getAllCourseWithNumber(String courseNumber)
+	{
+		CoursesExtractor extractor = new CoursesExtractor();
+		String queryTemplate = new String(QueryPathConstants.SEARCH_COURSE_NUMBER_QUERY);
+		MapSqlParameterSource parameters = new MapSqlParameterSource();
+		parameters.addValue("courseNumber", courseNumber);
+		List<Course> coursesWithNumberList = namedParameterJdbcTemplate.query(queryTemplate,parameters, extractor);
+		return coursesWithNumberList;
+	}
+	
+	@Override
+	public List<Section> getAllSectionsWithEndTime(int endTime)
+	{
+		SectionsExtractor extractor = new SectionsExtractor();
+		String queryTemplate = new String(QueryPathConstants.SEARCH_END_TIME_QUERY);
+		MapSqlParameterSource parameters = new MapSqlParameterSource();
+		parameters.addValue("endTime", endTime);
+		List<Section> sectionsWithEndTimeList = namedParameterJdbcTemplate.query(queryTemplate,parameters, extractor);
+		return sectionsWithEndTimeList;
+	}
+	
+	@Override
+	public List<Section> getAllSectionsWithExpectedPopulation(int expectedPopulation)
+	{
+		SectionsExtractor extractor = new SectionsExtractor();
+		String queryTemplate = new String(QueryPathConstants.SEARCH_EXPECTED_POPULATION_QUERY);
+		MapSqlParameterSource parameters = new MapSqlParameterSource();
+		parameters.addValue("expectedPopulation", expectedPopulation);
+		List<Section> sectionsWithExpectedPopulationList = namedParameterJdbcTemplate.query(queryTemplate,parameters, extractor);
+		return sectionsWithExpectedPopulationList;
+	}
+	
+	@Override
+	public List<User> getUser(String userName)
+	{
+		UsersExtractor extractor = new UsersExtractor();
+		String queryTemplate = new String(QueryPathConstants.SEARCH_USERS_QUERY);
+		MapSqlParameterSource parameters = new MapSqlParameterSource();
+		parameters.addValue("userName", userName);
+		List<User> usersList = namedParameterJdbcTemplate.query(queryTemplate,parameters, extractor);
+		return usersList;
+	}
 	
 }
