67ca5ce6f7f9ed1c9652b31e520c0c5a7a89ed4f
Michael Steidel <masteidel@gmail.com>
2018-02-08 17:28:08 -0500

    Michael - Begun work on getCourses and confirmSchedule

--- src/main/java/org/dselent/scheduling/server/controller/SchedulesController.java
+++ src/main/java/org/dselent/scheduling/server/controller/SchedulesController.java
@@ -3,7 +3,7 @@ package org.dselent.scheduling.server.controller;
 import java.util.Map;
 
 import org.dselent.scheduling.server.requests.GetCourses;
-import org.dselent.scheduling.server.requests.Register;
+import org.dselent.scheduling.server.requests.ConfirmSchedule;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,4 +13,6 @@ public interface SchedulesController {
 	@RequestMapping(method=RequestMethod.POST, value=GetCourses.REQUEST_NAME)
 	public ResponseEntity<String> getCourses(@RequestBody Map<String, String> request) throws Exception;
 
+	@RequestMapping(method=RequestMethod.POST, value=ConfirmSchedule.REQUEST_NAME)
+	public ResponseEntity<String> confirmSchedule(@RequestBody Map<String, String> request) throws Exception;
 }
