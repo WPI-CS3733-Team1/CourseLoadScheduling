2552db92fd25ac1799778734eed62a99d49ad700
dselent <selentd@gmail.com>
2018-02-11 17:31:26 -0500

    Fixed SQL query errors with incorrect parameter name format Fixed name error: see QueryPathConstants for the change

--- src/main/resources/sql/Active/SearchExpectedPopulation.sql
+++ src/main/resources/sql/Active/SearchExpectedPopulation.sql
@@ -1,7 +1,7 @@
 SELECT sections.crn, sections.section_name, sections.section_id, sections.expected_population, sections.academic_year, sections.academic_term, sections.start_time, sections.end_time, sections.days_per_week, sections.course_location, courses.course_abrv, courses.name, courses.dept_id, courses.course_number 
 FROM courses RIGHT OUTER JOIN sections
 ON courses.id = sections.course_id
-WHERE expected_population =: expectedPopulation
+WHERE expected_population = :expectedPopulation
 ORDER BY course_id ASC;
 
 
